e373c0123aba491bb3bb33f944ca47eea2a0caac
Hardik Khamele <hardikkhamele@gmail.com>
2026-05-03 14:31:01 +0530

    add layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 export default function Home() {
   return (
     <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>🚀 Bike49</h1>
+      <h1> Bike49 NEW</h1>
       <p>Buy & Sell Bikes Easily</p>
-
-      <div style={{ marginTop: "20px" }}>
-        <button style={{ marginRight: "10px" }}>Buy Bike</button>
-        <button>Sell Bike</button>
-      </div>
     </main>
   );
 }
