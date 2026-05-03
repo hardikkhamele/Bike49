c8d35f77210cda8d329c42704152807b84edff39
Hardik Khamele <hardikkhamele@gmail.com>
2026-05-03 14:27:18 +0530

    update homepage UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1>Bike49 Working </h1>;
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>🚀 Bike49</h1>
+      <p>Buy & Sell Bikes Easily</p>
+
+      <div style={{ marginTop: "20px" }}>
+        <button style={{ marginRight: "10px" }}>Buy Bike</button>
+        <button>Sell Bike</button>
+      </div>
+    </main>
+  );
 }
